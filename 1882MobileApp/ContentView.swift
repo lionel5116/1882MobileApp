b56868e2de469aafea3fa89effdf08309ee0ac13
Lionel Jones <lioneljones5116@gmail.com>
2026-06-27 19:11:00 -0500

    Initial Commit

---
+++ 1882MobileApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1882MobileApp
+//
+//  Created by Lionel Jones on 6/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1882MobileApp/_882MobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _882MobileAppApp.swift
+//  1882MobileApp
+//
+//  Created by Lionel Jones on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct _882MobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
